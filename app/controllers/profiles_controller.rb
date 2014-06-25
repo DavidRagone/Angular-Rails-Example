1cdc7eb6fbc675e276f9d37ace530405cba71494
DavidRagone <dmragone@goodreads.com>
2014-06-25 12:34:17 -0700

    Add sections to edit profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
   def edit
+    @user = User.first_or_create(first_name: 'Bob', email: 'bob@bob.bob')
+    @country_options = ['USA', 'Canada', 'some other place']
   end
 
   def update
